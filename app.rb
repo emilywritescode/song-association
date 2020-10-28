eff70e22986bc9f3d7961c315411a09a622af9bc
€mily <22375770+emilywritescode@users.noreply.github.com>
2020-10-27 23:25:14 -0400

    styling + word/count vars

--- app.rb
+++ app.rb
@@ -1,9 +1,61 @@
 require 'sinatra'
 
+enable :sessions
+
+
 get '/' do
     erb :index
 end
 
-get '/start' do
-    erb :start
+
+get '/game' do
+    @status_count = get_status_count()
+    @word = get_word()
+
+
+    erb :game
+end
+
+
+post '/game' do
+    # update count and redirect appropriately
+    @status_count = session[:status_count] + 1
+    session[:status_count] = @status_count
+
+    if @status_count == 11
+        puts "game is done"
+        session.clear
+        redirect '/'
+    else
+        redirect '/game'
+    end
+end
+
+
+def get_status_count()
+    if session[:status_count]  # get status count if it already exists
+        @status_count = session[:status_count]
+    else  # initialize status count
+        @status_count = 1
+        session[:status_count] = @status_count
+    end
+
+    @status_count
+end
+
+
+def get_words()
+    file_words = File.read("words.txt").split
+    @words = file_words.sample(10)
+    puts @words
+    @words
+end
+
+
+def get_word()
+    if !session[:words]
+        session[:words] = get_words()
+    end
+
+    session[:words][session[:status_count].to_i - 1]
 end
